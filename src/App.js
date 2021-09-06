b05db54a0cd42d6384ad2525a93a3bde5e9bf117
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-06 13:18:59 -0400

    adds construction gif to home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 // import face from "./images/portfolio_portait.png"
 import NavBar from "./components/NavBar"
+import construction from "./images/under_construction_giphy.webp"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <NavBar/>{/* < img src={face}></img> */}
+       < img src={construction}></img> 
       </header>
     </div>
   );
