2c942c0c6b63ce33aec616c2e92795d9eabf3cda
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-16 13:29:53 -0400

    fills out barebones of aboutme component

---
+++ src/components/AboutMe.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const AboutMe = () => {
+
+    return (
+        <div className = "about">
+            
+            
+
+        </div>
+
+      
+    )
+
+
+};
+ 
+  
+  export default AboutMe
