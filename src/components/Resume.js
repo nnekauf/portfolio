b7cd395e9d123e66eea7b86cf62cfff3408cf40b
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-16 13:31:23 -0400

    fills out barebones of Resume component

---
+++ src/components/Resume.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const Resume = () => {
+
+    return (
+        <div className = "resume">
+            
+            
+
+        </div>
+
+      
+    )
+
+
+};
+ 
+  
+  export default Resume
