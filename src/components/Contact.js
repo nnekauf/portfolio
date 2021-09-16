4684a0b6766a7c9aa51d15646fb0c5273d331a36
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-16 13:30:22 -0400

    fills out barebones of contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const Contact = () => {
+
+    return (
+        <div className = "contact">
+            
+            
+
+        </div>
+
+      
+    )
+
+
+};
+ 
+  
+  export default Contact
