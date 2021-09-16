712dc838ce8a7a92bc9dfa34f525ade79d0c06f2
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-16 13:23:36 -0400

    imports the needed components into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 // import face from "./images/portfolio_portait.png"
 import NavBar from "./components/NavBar"
+import AboutMe from "./components/AboutMe"
+import Contact from "./components/Contact"
+import Projects from "./components/Projects"
+import Resume from "./components/Resume"
+import Home from "./components/Home"
 import construction from "./images/under_construction_giphy.webp"
 import { Route, Switch, withRouter } from 'react-router-dom'
 
@@ -14,7 +19,8 @@ function App() {
            <Route exact path='/about' component={AboutMe}/>
            <Route exact path='/projects' component={Projects}/>
            <Route exact path='/resume' component={Resume}/>
-           <Route exact path='/contact' component={Signup}/>
+           <Route exact path='/contact' component={Contact}/>
+           
        </Switch>
 
        <h1> Please Excuse our Appearance</h1>
