19728783b97076e454d541e73670558daeab0217
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-16 13:30:57 -0400

    fills out barebones of Projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const Projects = () => {
+
+    return (
+        <div className = "projects">
+            
+            
+
+        </div>
+
+      
+    )
+
+
+};
+ 
+  
+  export default Projects
