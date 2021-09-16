3ca3b01e6fc3f7f2187bee018e8af8529d00037c
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-16 13:21:50 -0400

    imports route, switch, and with router in order to use naviagtion to each navbar link

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 // import face from "./images/portfolio_portait.png"
 import NavBar from "./components/NavBar"
 import construction from "./images/under_construction_giphy.webp"
+import { Route, Switch, withRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <NavBar/>{/* < img src={face}></img> */}
+       <Switch>
+           <Route exact path='/'component={Home}/>
+           <Route exact path='/about' component={AboutMe}/>
+           <Route exact path='/projects' component={Projects}/>
+           <Route exact path='/resume' component={Resume}/>
+           <Route exact path='/contact' component={Signup}/>
+       </Switch>
+
        <h1> Please Excuse our Appearance</h1>
        <div>This website is currently under construction</div>
        < img src={construction} alt= "Under Construction GIF"></img> 
