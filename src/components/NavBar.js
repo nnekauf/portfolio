c590b41a47513fea33e19a0a92eb3cd4b6573c0f
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-16 20:12:56 -0400

    makes the name and title portion a link to the home page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,13 +5,13 @@ const NavBar = () => {
     return (
         <div className = "NavBar">
             
-            <div>Nneka | Software Engineer</div> 
+            <div><Link className = "navButton"  to="/"> Nneka | Software Engineer</Link></div> 
             <div> 
                 <Link className = "navButton"  to="/about"> About Me |</Link>
                 <Link className = "navButton"  to="/projects"> Projects |</Link>  
                 <Link className = "navButton"  to="/resume"> Resume |</Link> 
                 <Link className = "navButton"  to="/contact"> Contact |</Link>
-                <Link className = "navButton"  to="/"> Home |</Link>
+                
             </div>
         </div>
     )
