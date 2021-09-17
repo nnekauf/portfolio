f553694372db4cd4c487ab2df30e71051d3ef19a
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-17 16:45:41 -0400

    adds HoopTown description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,6 +16,16 @@ const Projects = () => {
                 onMouseOut={event => event.target.pause()}
                 src={HoopTown}>
             </video>
+            <div className = "projectText">
+                HoopTown - <a className="aboutLink" href = "https://youtu.be/ExWmPQxtv6Q">Demo</a>
+                <ul>
+                <li>Street Tournament registration website that connects players and coaches nationwide. </li> 
+                <li>Utilizes Ruby on Rails as a backend API.</li>
+                <li>Handles user registration and personal accounts.</li>
+                <li>Successfully uses Redux to handle state management.</li>
+                <li>Designed frontend using pure CSS.</li>
+                </ul> 
+            </div>
             <br/>
             <video className="projectsVideo" width="480" height="480" 
                 onMouseOver={event => event.target.play()}
