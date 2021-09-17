3a46f75a98e5f99971207a3430c7fa2b5aa50f5a
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-16 22:18:53 -0400

    uses CSS to size and position portrait to the left and the text to the right

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,7 +7,14 @@ const AboutMe = () => {
 
     return (
         <div className = "about">
-            <div className= "face">< img src={face}></img></div>
+            <div className= "face" >
+                < img src={face} alt="Portrait of Nneka" 
+                    style={{width: "70%", height: "auto", 
+                    justifyContent: "left", position: "absolute",
+                    objectFit: "cover", objectPosition: "-150px", 
+                    zIndex: "1"}}>
+                </img>
+            </div>
                 <>
                     <Link className = "homeButton"  to="/projects">projects</Link> 
                     <br/> <br/>
@@ -16,12 +23,15 @@ const AboutMe = () => {
                     <Link className = "homeButton"  to="/contact">Contact</Link>
                 </>
             
-                <h1> Please Excuse our Appearance</h1>
+                <div style={{zIndex: "2", left: "50%", position: "relative"}}>
+                    <h1 > Please Excuse our Appearance</h1>
                     <div>This website is currently under construction</div>
                     <img src={construction} alt= "Under Construction GIF"></img> 
                     <div>No worries, we are working quickly to deliver an amazing website to you. 
                         Please check back soon.
                     </div>
+                </div>
+                
         
 
         </div>
