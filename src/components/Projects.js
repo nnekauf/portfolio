22d0a18b2e5608db3d9581c17bc876260244fc17
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-17 16:06:35 -0400

    adds videos of projects into Project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import construction from "../images/under_construction_giphy.webp"
+import HoopTown from "../images/HoopTown-screen-recording.mov"
+import Nest from "../images/Nest-screen-recording.mov"
+import MyNba from "../images/MyNba-screen-recording.mov"
 
 
 const Projects = () => {
@@ -8,6 +11,21 @@ const Projects = () => {
         <div className = "projects">
             <text className="mainTitle"> Projects Page</text> <br/>
             <h1 > Please Excuse our Appearance</h1>
+            <video className="projectsVideo" width="480" height="480" 
+                onMouseOver={event => event.target.play()}
+                onMouseOut={event => event.target.pause()}
+                src={HoopTown}>
+            </video>
+            <video className="projectsVideo" width="480" height="480" 
+                onMouseOver={event => event.target.play()}
+                onMouseOut={event => event.target.pause()}
+                src={Nest}>
+            </video>
+            <video className="projectsVideo" width="480" height="480" 
+                onMouseOver={event => event.target.play()}
+                onMouseOut={event => event.target.pause()}
+                src={MyNba}>
+            </video>
                 <div>This website is currently under construction</div>
                 <img id="construction" src={construction} alt= "Under Construction GIF"></img> 
                 <div>No worries, we are working quickly to deliver an amazing website to you. 
