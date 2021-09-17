aa9df21cde35db2d4ccd3b31f9f5b5e151a1c1e5
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-17 18:14:40 -0400

    fills bare bones of resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import construction from "../images/under_construction_giphy.webp"
 
 
 
@@ -7,13 +6,25 @@ const Resume = () => {
 
     return (
         <div className = "resume">
-                <h1 > Please Excuse our Appearance</h1>
-                <div>This website is currently under construction</div>
-                <img id="construction" src={construction} alt= "Under Construction GIF"></img>  
-                <div>No worries, we are working quickly to deliver an amazing website to you. 
-                    Please check back soon.
-                </div>
+            <div className="resumeCard">
+            Programming Languages: Javascript, ES6, Ruby, HTML, CSS; 
+            Frameworks:  Rails, React, Redux;
+            Database Management Software: SQL, PostgreSQL, SQLite, Active Record, Sinatra, RESTful API; 
+            Tools: Git, Heroku, Node, Adobe Creative Cloud, Zendesk
+
+            </div>
             
+            <div className="resumeCard">
+
+            </div>
+
+            <div className="resumeCard">
+
+            </div>
+
+            <div className="resumeCard">
+
+            </div>
 
         </div>
 
