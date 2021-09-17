39617a79fbeaeb670383922d34846b444953da1a
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-17 16:14:07 -0400

    fixes margin spacing and font

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import construction from "../images/under_construction_giphy.webp"
 import HoopTown from "../images/HoopTown-screen-recording.mov"
 import Nest from "../images/Nest-screen-recording.mov"
 import MyNba from "../images/MyNba-screen-recording.mov"
@@ -9,28 +8,27 @@ const Projects = () => {
 
     return (
         <div className = "projects">
-            <text className="mainTitle"> Projects Page</text> <br/>
-            <h1 > Please Excuse our Appearance</h1>
+            <h1 className="mainTitle"> Projects</h1> 
+            <br/>
+            
             <video className="projectsVideo" width="480" height="480" 
                 onMouseOver={event => event.target.play()}
                 onMouseOut={event => event.target.pause()}
                 src={HoopTown}>
             </video>
+            <br/>
             <video className="projectsVideo" width="480" height="480" 
                 onMouseOver={event => event.target.play()}
                 onMouseOut={event => event.target.pause()}
                 src={Nest}>
             </video>
+            <br/>
             <video className="projectsVideo" width="480" height="480" 
                 onMouseOver={event => event.target.play()}
                 onMouseOut={event => event.target.pause()}
                 src={MyNba}>
             </video>
-                <div>This website is currently under construction</div>
-                <img id="construction" src={construction} alt= "Under Construction GIF"></img> 
-                <div>No worries, we are working quickly to deliver an amazing website to you. 
-                    Please check back soon.
-                </div>
+                
 
         </div>
 
