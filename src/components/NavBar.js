14b703d119fd05cdd88566baf4bdb1530672ef8b
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-16 21:33:41 -0400

    styles navBar with css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,14 @@ const NavBar = () => {
     return (
         <div className = "NavBar">
             
-            <div style={{textAlign: "left", fontFamily: "Bangers"}}>
-                <Link className = "navTitle" to="/"> Nneka | Software Engineer</Link>
+            <div style={{fontFamily: "Bangers", fontSize: "2em"}}>
+                <Link className = "navLink" to="/"> Nneka | Software Engineer</Link>
             </div> 
-            <div style={{fontFamily: "OpenSans", fontWeight: "bold"}}> 
-                <Link className = "navLink"  to="/"> About Me |</Link>
-                <Link className = "navLink"  to="/projects"> Projects |</Link>  
-                <Link className = "navLink"  to="/resume"> Resume |</Link> 
-                <Link className = "navLink"  to="/contact"> Contact |</Link>
+            <div style={{fontFamily: "OpenSans", fontWeight: "lighter"}}> 
+                <Link className = "navLink"  to="/"> About Me</Link>
+                <Link className = "navLink"  to="/projects"> Projects</Link>  
+                <Link className = "navLink"  to="/resume"> Resume</Link> 
+                <Link className = "navLink"  to="/contact"> Contact</Link>
                 
             </div>
         </div>
