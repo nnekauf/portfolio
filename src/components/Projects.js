03d70ff40a560685a8a42a23145e877758c14116
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-17 16:48:58 -0400

    adds Nest  description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,22 +16,35 @@ const Projects = () => {
                 onMouseOut={event => event.target.pause()}
                 src={HoopTown}>
             </video>
+
             <div className = "projectText">
                 HoopTown - <a className="aboutLink" href = "https://youtu.be/ExWmPQxtv6Q">Demo</a>
+                <br/>Street Tournament registration website that connects players and coaches nationwide.
                 <ul>
-                <li>Street Tournament registration website that connects players and coaches nationwide. </li> 
                 <li>Utilizes Ruby on Rails as a backend API.</li>
                 <li>Handles user registration and personal accounts.</li>
                 <li>Successfully uses Redux to handle state management.</li>
                 <li>Designed frontend using pure CSS.</li>
                 </ul> 
             </div>
+
             <br/>
             <video className="projectsVideo" width="480" height="480" 
                 onMouseOver={event => event.target.play()}
                 onMouseOut={event => event.target.pause()}
                 src={Nest}>
             </video>
+
+            <div className = "projectText">
+                Nest - <a className="aboutLink" href = "https://www.youtube.com/watch?v=llJWNpN7jBQ">Demo</a>
+                <br/>Book review website that fosters conversations between readers and authors.
+                <ul>
+                <li>Utilizes Ruby on Rails for backend and front end.</li> 
+                <li>Enables users to create a reader or author account.</li>
+                <li>Designed frontend using pure CSS.</li>
+
+                </ul> 
+            </div>
             <br/>
             <video className="projectsVideo" width="480" height="480" 
                 onMouseOver={event => event.target.play()}
