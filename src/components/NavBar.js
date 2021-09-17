642e9b497273359b2cb8657f9b20447e68e342da
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-16 20:11:39 -0400

    adds home link to NAvBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,18 @@
 import React from "react"
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <div className = "NavBar">
-            <div>Nneka | Software Engineer</div> <div>About Me | Projects | Resume | Contact</div>
+            
+            <div>Nneka | Software Engineer</div> 
+            <div> 
+                <Link className = "navButton"  to="/about"> About Me |</Link>
+                <Link className = "navButton"  to="/projects"> Projects |</Link>  
+                <Link className = "navButton"  to="/resume"> Resume |</Link> 
+                <Link className = "navButton"  to="/contact"> Contact |</Link>
+                <Link className = "navButton"  to="/"> Home |</Link>
+            </div>
         </div>
     )
 }
